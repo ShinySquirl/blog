b7b10ab81a1a9aa8ad1c697971560c998d0e9cca
Nic Baird <nicbaird@macbook-air-3.lan>
2024-05-24 16:17:29 -0700

    added a bunch of random CSS and testing, none of which worked

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "jekyll"
+gem "minimal-mistakes-jekyll"
+gem "jekyll-sitemap"
+gem "jekyll-seo-tag"
+gem "jekyll-feed"
+gem "jekyll-archives"
+gem "jekyll-paginate"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "A Year of Exploration",
+        "excerpt":"Began on July 1, 2023 I thought because I’d seen the investor side for a year, that it would be easy to complete the playbook. I was wrong. Sustainable Shopping For the first several months I spun my wheels and went nowhere. I stayed on a bad idea for far...","categories": [],
+        "tags": [],
+        "url": "/blog/2024/05/24/a-year-of-exploration.html",
+        "teaser": null
+      }]
